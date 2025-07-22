778bab560b9a04856a66fab7d4ce0c4f3d95b0f8
Karthika Krishna K <karthikkunjarakana@gmail.com>
2025-07-22 11:03:15 +0530

    palindrome

--- Java/Strings/Palindrome.java
+++ Java/Strings/Palindrome.java
@@ -1,19 +1,34 @@
-package StringPlay;
-
+// This program checks if a given string is a palindrome or not.
 public class Palindrome {
 public static void main(String[] args) {
+	//StringBuilder + reverse() method
 	String s="level";
 	StringBuilder str= new StringBuilder(s); // 
 	String rev = str.reverse().toString();
 	System.out.println(rev);
 	System.out.println(rev.equals(s)?"Palindrome":"Not a Palindrome");
 	
-	
+	//Primitive method to trace and do it.
 	String newString= "";
+	
 	for (int i = s.length()-1; i >=0; i--) {
 		newString+=s.charAt(i);
 	}
+	
 	System.out.println(newString);
 	System.out.println(newString.equals(s)?"Palindrome":"Not a Palindrome");
+	
+	System.out.println(reverser(s));
+	System.out.println(reverser(s).equals(s)?"Palindrome":"Not a Palindrome");
+
+}
+
+static String reverser(String str) {
+	StringBuilder reverseString = new StringBuilder();
+	char arr[]= str.toCharArray();
+	for(int i= arr.length-1;i>=0;i--) {
+		reverseString.append(arr[i]);
+	}
+	return reverseString.toString();
 }
 }
